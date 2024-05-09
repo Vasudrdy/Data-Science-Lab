88863e0eea7bd9a5fff4175170520c7dd2f846b3
Vasudrdy <120782021+Vasudrdy@users.noreply.github.com>
2024-05-09 12:20:18 +0530

    Create 1st program.py

---
+++ 1st program.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+hours_studied = [10, 9, 2, 15, 10, 16, 11, 16]
+exam_scores = [95, 80, 10, 50, 45, 98, 38, 93]
+plt.plot(hours_studied, exam_scores, marker='*', color='red', linestyle='-')
+plt.xlabel('Hours Studied')
+plt.ylabel('Score in Final Exam')
+plt.title('Effect of Hours Studied on Exam Score')
+plt.show()
